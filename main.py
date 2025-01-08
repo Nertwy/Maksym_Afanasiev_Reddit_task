f64b2be80da3ca9a1e17892117818210ba31a568
Nertwy <deniskabos@gmail.com>
2025-01-08 17:36:24 +0200

    remove docstrings, add blocks

--- main.py
+++ main.py
@@ -22,22 +22,18 @@ class RedditAPIClient:
         )
         self.xlsxclient = ExcelHandler(read_file_path, write_file_path)
 
-    def get_submission_comments(self, submission_url: str, trafic: str):
+    def get_submission_comments(self, submission_url: str, traffic: str):
         submission = self.reddit.submission(url=submission_url)
-        is_locked = submission.locked
-        is_archived = submission.archived
-        if is_locked or is_archived:
+        if submission.locked or submission.archived:
             return
-        comments = submission.comments
-        comments_count = len(comments)
+        comments_count = len(submission.comments)
+        self._write_comments_to_excel(submission_url, comments_count, traffic)
+
+    def _write_comments_to_excel(self, submission_url: str, comments_count: int, traffic: str):
         if comments_count == 0:
-            self.xlsxclient.write_data_to_sheet(
-                [submission_url, 0, trafic], "No comments")
+            self.xlsxclient.write_data_to_sheet([submission_url, 0, traffic], "No comments")
         elif 1 <= comments_count <= 3:
-            self.xlsxclient.write_data_to_sheet(
-                [submission_url, comments_count, trafic], "3 or less comments")
-        else:
-            pass
+            self.xlsxclient.write_data_to_sheet([submission_url, comments_count, traffic], "3 or less comments")
 
     def run(self, skip_header: bool = True):
         data = self.xlsxclient.read_data()
@@ -45,10 +41,10 @@ class RedditAPIClient:
             return
         for sheet_name, sheet_data in data.items():
             for index, row in enumerate(sheet_data):
-                url, trafic = row
                 if skip_header and index == 0:
                     continue
-                self.get_submission_comments(url, trafic)
+                url, traffic = row
+                self.get_submission_comments(url, traffic)
                 logger.info(f"Processed: row {index} in sheet {sheet_name}")
         self.xlsxclient.sort_output_by_traffic()
         logger.info("Processing complete!")
@@ -62,7 +58,6 @@ class ExcelHandler:
         self.write_workbook = Workbook()
 
     def read_data(self):
-        """Reads data from the read Excel file and returns a dictionary of sheet data."""
         try:
             self.read_workbook = load_workbook(self.read_file_path)
             data = {}
@@ -75,26 +70,21 @@ class ExcelHandler:
         except FileNotFoundError:
             logger.error(f"Error: File '{self.read_file_path}' not found.")
         except InvalidFileException:
-            logger.error(
-                "Error: Invalid file format. Please provide an Excel file.")
+            logger.error("Error: Invalid file format. Please provide an Excel file.")
         except Exception as e:
             logger.error(f"Error reading file: {e}")
 
     def write_data_to_sheet(self, row_data: list, sheet_name):
-        if sheet_name in self.write_workbook.sheetnames:
-            sheet = self.write_workbook[sheet_name]
-        else:
-            sheet = self.write_workbook.create_sheet(sheet_name)
+        sheet = self.write_workbook[sheet_name] if sheet_name in self.write_workbook.sheetnames else self.write_workbook.create_sheet(sheet_name)
+        if sheet.max_row == 1:
             sheet.append(["URL", "Number of comments", "Traffic"])
         sheet.append(row_data)
-
         self.write_workbook.save(self.write_file_path)
 
     def sort_output_by_traffic(self):
         for sheet_name in self.write_workbook.sheetnames:
             sheet = self.write_workbook[sheet_name]
-            sorted_data = sorted(sheet.iter_rows(
-                min_row=2, values_only=True), key=lambda x: x[2], reverse=True)
+            sorted_data = sorted(sheet.iter_rows(min_row=2, values_only=True), key=lambda x: x[2], reverse=True)
             sheet.delete_rows(2, sheet.max_row)
             for row in sorted_data:
                 sheet.append(row)
@@ -104,6 +94,5 @@ class ExcelHandler:
 if __name__ == "__main__":
     input_read_file_path = sys.argv[1]
     output_file_path = sys.argv[2]
-    reddit_script = RedditAPIClient(
-        read_file_path=input_read_file_path, write_file_path=output_file_path)
+    reddit_script = RedditAPIClient(read_file_path=input_read_file_path, write_file_path=output_file_path)
     reddit_script.run()
